185d58ef66fb42abb4cae7581a49c9e07c9b8286
73nko <apramos89@gmail.com>
2021-04-17 11:15:08 +0200

    Moved to snowpack

---
+++ snowpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    mount: {
+        assets: "/",
+        src: "/",
+    },
+    plugins: [
+        ['@snowpack/plugin-sass', { style: 'compressed'}],
+        [
+            'snowpack-plugin-imagemin',
+            {
+              include: ['**/*.jpg', '**/*.png'],
+              plugins: [
+                require('imagemin-mozjpeg')({quality: 90, progressive: true}),
+                require('imagemin-optipng')({optimizationLevel: 7}),
+              ],
+            },
+        ],
+    ],
+    buildOptions: {
+        out: "../dist",
+        clean: true,
+        minify: true,
+    },
+    exclude: [
+        "**/node_modules/**/*",
+    ],
+    optimize: {
+        /* Use built-in esbuild bundling for build */
+        bundle: true,
+        minify: true,
+        target: "es2020",
+        treeshake: true,
+        splitting: true,
+      },
+  };
