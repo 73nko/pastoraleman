e87d0dd225b75505946e221bdb14309d64bc3505
Alex Perez <apramos89@gmail.com>
2021-12-20 08:19:02 +0100

    Small updates

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,44 +1,42 @@
 module.exports = {
-    mount: {
-        "src": "/",
-        "public": "/"
-    },
-    plugins: [
-        ['@snowpack/plugin-sass', { style: 'compressed'}],
-        [
-            'snowpack-plugin-imagemin',
-            {
-              include: ['**/*.jpg', '**/*.png'],
-              plugins: [
-                require('imagemin-mozjpeg')({quality: 90, progressive: true}),
-                require('imagemin-optipng')({optimizationLevel: 7}),
-              ],
-            },
+  mount: {
+    src: "/",
+    public: "/",
+  },
+  plugins: [
+    ["@snowpack/plugin-sass", { style: "compressed" }],
+    [
+      "snowpack-plugin-imagemin",
+      {
+        include: ["**/*.jpg", "**/*.png"],
+        plugins: [
+          require("imagemin-mozjpeg")({ quality: 80, progressive: true }),
+          require("imagemin-optipng")({ optimizationLevel: 7 }),
         ],
-        [
-            'snowpack-plugin-resize-images',
-            {
-                '**/img/**': {
-                    webp: {
-                      quality: 90,
-                    },
-                  },
-            }
-        ]
+      },
     ],
-    buildOptions: {
-        clean: true,
-        minify: true,
-    },
-    exclude: [
-        "**/node_modules/**/*",
+    [
+      "snowpack-plugin-resize-images",
+      {
+        "**/img/**": {
+          webp: {
+            quality: 80,
+          },
+        },
+      },
     ],
-    optimize: {
-        /* Use built-in esbuild bundling for build */
-        bundle: true,
-        minify: true,
-        target: "es2020",
-        treeshake: true,
-        splitting: true,
-    },
-  };
+  ],
+  buildOptions: {
+    clean: true,
+    minify: true,
+  },
+  exclude: ["**/node_modules/**/*"],
+  optimize: {
+    /* Use built-in esbuild bundling for build */
+    bundle: true,
+    minify: true,
+    target: "es2020",
+    treeshake: true,
+    splitting: true,
+  },
+};
